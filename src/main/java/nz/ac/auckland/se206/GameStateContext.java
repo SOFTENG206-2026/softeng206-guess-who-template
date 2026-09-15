6967dea721c170cb7c0d07a090f09a8da4dcab10
valerio-terragni <66171778+valerio-terragni@users.noreply.github.com>
2026-09-15 12:21:58 +1200

    Decrease initial elapsedSeconds from 120 to 118

--- src/main/java/nz/ac/auckland/se206/GameStateContext.java
+++ src/main/java/nz/ac/auckland/se206/GameStateContext.java
@@ -44,7 +44,7 @@ public class GameStateContext {
   /** Constructs a new GameStateContext and initializes the game states and professions. */
   public GameStateContext() {
 
-    elapsedSeconds = new SimpleIntegerProperty(120);
+    elapsedSeconds = new SimpleIntegerProperty(118);
     KeyFrame oneSecond =
         new KeyFrame(
             Duration.seconds(1),
